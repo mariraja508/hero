c4359568b36d01f87e0dad076b6161139de2701c
mariraja508 <82996325+mariraja508@users.noreply.github.com>
2021-05-20 12:06:16 +0530

    Add files via upload

---
+++ hrotest.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const body=require('body-parser')
+const fs = require("fs")
+app.use(body())
+app.use(cors())
+app.post('/', function(req, res) {
+    res.send({name : 'Mariraja'})
+}
+
+app.listen(5000);
